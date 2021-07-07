ed3f972234f89ea3539702abab52962ca648a0d1
juo <juo6442@gmail.com>
2021-07-08 01:47:15 +0900

    Change viewport size

--- src/game/Environment.ts
+++ src/game/Environment.ts
@@ -1,6 +1,6 @@
 export default class {
     static readonly FPS = 60;
     static readonly VIEWPORT_WIDTH = 1024;
-    static readonly VIEWPORT_HEIGHT = 768;
+    static readonly VIEWPORT_HEIGHT = 576;
     static readonly DEBUG = true;
 }
