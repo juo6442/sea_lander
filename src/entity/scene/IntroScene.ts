26a4562147019d0cfa055f8fbb34b1f4ee83a22c
juo <juo6442@gmail.com>
2021-07-08 01:17:29 +0900

    Fix incorrect path

--- src/entity/scene/IntroScene.ts
+++ src/entity/scene/IntroScene.ts
@@ -1,20 +1,19 @@
 import Resource from "../../game/Resource";
+import Logger from "../../util/Logger";
 import { SceneEntity, SceneManager } from "./Scene";
 
 export default class IntroScene extends SceneEntity {
     constructor(manager: SceneManager) {
         super(manager);
 
-        /*
         new Resource.Loader()
                 .setFont("NeoDgm")
-                .setImage("play", "play.png")
+                .setImage("play", "sprite/play.png")
                 .load()
                 .then(resource => this.onResourceLoad(resource));
-                */
     }
 
     private onResourceLoad(resource: Resource): void {
-        // TODO: make entities
+        Logger.info("IntroScene is loaded");
     }
 }
