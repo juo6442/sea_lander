0033c019ff25c27b0dcc70b2f20bd6f323a9c55c
juo <juo6442@gmail.com>
2021-07-08 02:37:48 +0900

    Change screen size

--- src/game/Environment.ts
+++ src/game/Environment.ts
@@ -1,7 +1,7 @@
 export default class {
     static readonly FPS = 60;
     static readonly VIEWPORT_WIDTH = 1024;
-    static readonly VIEWPORT_HEIGHT = 576;
+    static readonly VIEWPORT_HEIGHT = 768;
     static readonly FONT_DEFAULT = "NeoDgm";
     static readonly DEBUG = true;
 }
