f903c085792f3daa89a66944311fafa3bb3277ab
juo <juo6442@gmail.com>
2021-07-09 01:36:32 +0900

    Move Script

---
+++ src/script/Script.ts
@@ -0,0 +1,40 @@
+import { Color, Position, Size } from "../entity/Entity";
+import { KeyStatus } from "../game/KeyInput";
+
+export default abstract class Script {
+    private _finished: boolean;
+
+    constructor() {
+        this._finished = false;
+    }
+
+    get finished(): boolean {
+        return this._finished;
+    }
+
+    /**
+     * Updates its status per each frame.
+     * `this.finish()` should be called here when finished.
+     * @param keyStatus - Key status
+     */
+    public abstract update(keyStatus: KeyStatus): void;
+
+    /**
+     * Finishes the script execution. Its owner may remove it.
+     */
+    protected finish(): void {
+        this._finished = true;
+    }
+}
+
+export interface PositionEntity {
+    position: Position;
+}
+
+export interface SizeEntity {
+    size: Size;
+}
+
+export interface ColoredEntity {
+    color: Color;
+}
