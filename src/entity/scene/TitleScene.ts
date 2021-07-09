91e6f730a674bdae1b9770c96b25784943ac4459
juo <juo6442@gmail.com>
2021-07-10 02:21:10 +0900

    Implement some TitleScene

--- src/entity/scene/TitleScene.ts
+++ src/entity/scene/TitleScene.ts
@@ -1,4 +1,5 @@
 import Environment from "../../game/Environment";
+import { Key, KeyStatus } from "../../game/KeyInput";
 import Resource from "../../game/Resource";
 import Logger from "../../util/Logger";
 import Label, { TextAlign } from "../Label";
@@ -7,19 +8,53 @@ import Sprite from "../Sprite";
 import Scene, { Bundle, SceneManager } from "./Scene";
 
 export default class TitleScene extends Scene {
+    private resource: Resource;
+    private isWaitingInput: boolean;
+
+    constructor(sceneManager: SceneManager, bundle?: Bundle) {
+        super(sceneManager, bundle);
+
+        this.resource = this.getFromBundle("resource");
+        this.isWaitingInput = false;
+    }
+
     public start(): void {
         Logger.info("Start TitleScene");
 
-    }
+        this.resource = this.getFromBundle("resource") as Resource;
 
-    private onResourceLoad(resource: Resource): void {
-        Logger.info("TitleScene is loaded");
 
         this.addEntity("rect_bg", new Rect.Builder()
                 .setSizeFullscreen()
-                .setColor(0, 0, 0)
+                .setColor(255, 255, 255)
                 .setAlignCenter(false)
                 .setPosition(0, 0)
                 .build());
+        /*
+        TODO: Show title images
+        TODO: Show scoreboard (highlight latest score)
+        */
+
+        this.addEntity("label_pressEnter", new Label.Builder()
+                .setAlign(TextAlign.ALIGN_CENTER)
+                .setColor(0, 0, 0)
+                .setText("엔터를 누르면 시작합니다")
+                .setSize(30)
+                .setPosition(Environment.VIEWPORT_WIDTH / 2, Environment.VIEWPORT_HEIGHT / 2)
+                .build());
+    }
+
+    public override update(keyStatus: KeyStatus) {
+        super.update(keyStatus);
+
+        if (this.isWaitingInput) this.waitOkKey(keyStatus);
+    }
+
+    private waitOkKey(keyStatus: KeyStatus): void {
+        if (keyStatus.isPressed(Key.OK)) {
+            this.isWaitingInput = false;
+
+            // TODO: Switch to PlayScene
+        }
     }
 }
