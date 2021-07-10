d74eb50ef60e9f2006cdaef6254dc65162f3a38b
juo <juo6442@gmail.com>
2021-07-10 15:31:21 +0900

    Add SEA head to scene

--- src/entity/scene/InGameScene.ts
+++ src/entity/scene/InGameScene.ts
@@ -1,18 +1,24 @@
 import Environment from "../../game/Environment";
-import { Key, KeyStatus } from "../../game/KeyInput";
+import { KeyStatus } from "../../game/KeyInput";
+import PlayerStatus from "../../game/PlayerStatus";
 import Resource from "../../game/Resource";
 import Logger from "../../util/Logger";
+import SeaHead from "../actor/SeaHead";
+import { Position } from "../Entity";
 import Rect from "../Rect";
 import Sprite from "../Sprite";
 import Scene, { Bundle, SceneManager } from "./Scene";
 
 export default class InGameScene extends Scene {
     private resource: Resource;
+    private playerStatus: PlayerStatus;
+    private seaHead: SeaHead | undefined;
 
     constructor(sceneManager: SceneManager, bundle?: Bundle) {
         super(sceneManager, bundle);
 
         this.resource = Resource.global!;
+        this.playerStatus = new PlayerStatus();
     }
 
     public start(): void {
@@ -24,6 +30,15 @@ export default class InGameScene extends Scene {
                 .setImage(this.resource.getImage("room"))
                 .setSize(Environment.VIEWPORT_WIDTH, Environment.VIEWPORT_HEIGHT)
                 .build());
+
+        this.initGame();
+    }
+
+    public initGame(): void {
+        this.seaHead = new SeaHead(
+                this.playerStatus,
+                new Position(Environment.VIEWPORT_WIDTH / 2, 0));
+        this.addEntity("actor_seahead", this.seaHead);
     }
 
     public override update(keyStatus: KeyStatus): void {
