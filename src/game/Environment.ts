d29aba64250a10fba031b48f27d46283e495434c
juo <juo6442@gmail.com>
2021-07-16 02:12:58 +0900

    Set debug to false

--- src/game/Environment.ts
+++ src/game/Environment.ts
@@ -3,5 +3,5 @@ export default class {
     static readonly VIEWPORT_WIDTH = 2048;
     static readonly VIEWPORT_HEIGHT =  1536;
     static readonly FONT_DEFAULT = "NeoDgm";
-    static readonly DEBUG = true;
+    static readonly DEBUG = false;
 }
